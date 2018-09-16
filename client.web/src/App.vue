aecb344a46bf637d02e485042aef0bb9b7f7a500
luiscarbonell <luiscarbonell5683@gmail.com>
2018-09-16 19:46:26 +0000

    Latest Changes

--- client.web/src/App.vue
+++ client.web/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
+<!--     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/elements">Elements</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
@@ -16,6 +16,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100vh;
 }
 #nav {
   padding: 30px;
--- client.web/src/views/Home.vue
+++ client.web/src/views/Home.vue
@@ -2,18 +2,55 @@
   <div class="home">
 <!--     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <h1>hello</h1>
+    <header class="header">
+      <img src="../assets/Logo.png" alt="SoFlo Tech Logo" class="logo">
+      <nav>
+        <ul class="nav">
+          <li><BaseLink href="">Dicks</BaseLink></li>
+          <li><BaseLink href="">Dicks</BaseLink></li>
+          <li><BaseLink href="">Dicks</BaseLink></li>
+          <li><BaseLink href="">Dicks</BaseLink></li>
+          <li><BaseLink href="">Dicks</BaseLink></li>
+        </ul>
+      </nav>
+    </header>
+<!--     <img src="../assets/Header.png" alt="SoFlo Tech Banner" class="banner"> -->
+<!--     <h1>hello</h1> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import BaseLink from '@/components/BaseLink.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
+    BaseLink,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  * {
+    font-family: "Roboto", "Nunito";
+  }
+  header.header {
+    background-image: url('../assets/Header.png');
+    background-position: bottom;
+    height: 100vh;
+  }
+  header img.logo {
+    width: 1100px;
+    padding-top: 35px;
+  }
+  .nav {
+    display:flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    list-style: none;
+    border: 1px solid banner;
+  }
+</style>
